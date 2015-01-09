7fef1ec633ef4f265b064c82ad6759062f73dc3a
Shannon Beck <Shannon.e.beck@gmail.com>
2015-01-09 09:11:58 -0800

    Initial Commit

---
+++ WeekOneAssessment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeekOneAssessment
+//
+//  Created by Shannon Beck on 1/9/15.
+//  Copyright (c) 2015 Shannon Beck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeekOneAssessment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeekOneAssessment
+//
+//  Created by Shannon Beck on 1/9/15.
+//  Copyright (c) 2015 Shannon Beck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
